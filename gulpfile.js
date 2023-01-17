7f3c7d6c153d470f091f39c619ab9876e395393a
Niziol <niziol.luk@gmail.com>
2023-01-17 17:37:57 +0100

    created site structure with html contents

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+const { src, dest, series, parallel, watch } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('gulp-cssnano');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const sourcemaps = require('gulp-sourcemaps');
+const clean = require('gulp-clean');
+const kit = require('gulp-kit');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+
+const paths = {
+	html: './*.html',
+	htmlKit: './html/**/*.kit',
+	sass: './src/sass/**/*.scss',
+	js: './src/js/**/*.js',
+	img: './src/img/*',
+	dist: './dist',
+	sassDest: './dist/css',
+	jsDest: './dist/js',
+	imgDest: './dist/img',
+};
+
+function sassCompiler() {
+	return src(paths.sass)
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.init())
+		.pipe(autoprefixer())
+		.pipe(cssnano())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(sourcemaps.write())
+		.pipe(dest(paths.sassDest));
+}
+
+function javaScript() {
+	return src(paths.js)
+		.pipe(sourcemaps.init())
+		.pipe(babel({ presets: ['@babel/env'] }))
+		.pipe(uglify())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(sourcemaps.write())
+		.pipe(dest(paths.jsDest));
+}
+
+function convertImages() {
+	return src(paths.img).pipe(imagemin()).pipe(dest(paths.imgDest));
+}
+
+function handleKits() {
+	return src(paths.htmlKit).pipe(kit()).pipe(dest('./'));
+}
+
+function cleanDist() {
+	return src(paths.dist, { read: false }).pipe(clean());
+}
+
+function startBrowserSync(done) {
+	browserSync.init({
+		server: {
+			baseDir: './',
+		},
+	});
+	done();
+}
+
+function watchForChanges(done) {
+	watch(paths.html).on('change', reload);
+	watch(
+		[paths.htmlKit, paths.sass, paths.js],
+		parallel(handleKits, sassCompiler, javaScript)
+	).on('change', reload);
+	watch(paths.img, convertImages).on('change', reload);
+	done();
+}
+
+const mainFunctions = parallel(
+	handleKits,
+	sassCompiler,
+	javaScript,
+	convertImages
+);
+
+exports.cleanDist = cleanDist;
+exports.default = series(mainFunctions, startBrowserSync, watchForChanges);
